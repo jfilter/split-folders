ac65cd30eaf2126aa16714bfb73100fc3829caf8
Johannes Filter <hi@jfilter.de>
2018-10-04 23:20:34 +0200

    drop 3.5 support

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ with open("README.md", "r") as fh:
 
 classifiers = [
     'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'License :: OSI Approved :: MIT License',
