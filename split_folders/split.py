d41d9d459386fb48e1fb85f41b141ac70bbb43dd
Johannes Filter <hi@jfilter.de>
2019-07-30 20:22:55 +0200

    format with black

--- split_folders/split.py
+++ split_folders/split.py
@@ -54,10 +54,14 @@ def list_dirs(directory):
 def list_files(directory):
     """Returns all files in a given directory
     """
-    return [f for f in pathlib.Path(directory).iterdir() if f.is_file() and not f.name.startswith('.')]
+    return [
+        f
+        for f in pathlib.Path(directory).iterdir()
+        if f.is_file() and not f.name.startswith(".")
+    ]
 
 
-def ratio(input, output="output", seed=1337, ratio=(.8, .1, .1)):
+def ratio(input, output="output", seed=1337, ratio=(0.8, 0.1, 0.1)):
     # make up for some impression
     assert round(sum(ratio), 5) == 1
     assert len(ratio) in (2, 3)
@@ -75,7 +79,7 @@ def ratio(input, output="output", seed=1337, ratio=(.8, .1, .1)):
 def fixed(input, output="output", seed=1337, fixed=(100, 100), oversample=False):
     # make sure its reproducible
     if isinstance(fixed, int):
-        fixed = (fixed)
+        fixed = fixed
 
     assert len(fixed) in (1, 2)
 
@@ -99,11 +103,11 @@ def fixed(input, output="output", seed=1337, fixed=(100, 100), oversample=False)
         zip(lens, dirs), desc="Oversampling", unit=" classes"
     ):
         class_name = path.split(class_dir)[1]
-        full_path = path.join(output, 'train', class_name)
+        full_path = path.join(output, "train", class_name)
         train_files = list_files(full_path)
         for i in range(max_len - length):
             f_orig = random.choice(train_files)
-            new_name = f_orig.stem + '_' + str(i) + f_orig.suffix
+            new_name = f_orig.stem + "_" + str(i) + f_orig.suffix
             f_dest = f_orig.with_name(new_name)
             shutil.copy2(f_orig, f_dest)
 
@@ -127,7 +131,9 @@ def split_class_dir_fixed(class_dir, output, fixed, seed, prog_bar):
     files = setup_files(class_dir, seed)
 
     if not len(files) > sum(fixed):
-        raise ValueError(f'The number of samples in class "{class_dir.stem}" are too few. There are only {len(files)} samples available but your fixed parameter {fixed} requires at least {sum(fixed)} files. You may want to split your classes by ratio.')
+        raise ValueError(
+            f'The number of samples in class "{class_dir.stem}" are too few. There are only {len(files)} samples available but your fixed parameter {fixed} requires at least {sum(fixed)} files. You may want to split your classes by ratio.'
+        )
 
     split_train = len(files) - sum(fixed)
     split_val = split_train + fixed[0]
@@ -155,12 +161,12 @@ def split_files(files, split_train, split_val, use_test):
     files_train = files[:split_train]
     files_val = files[split_train:split_val] if use_test else files[split_train:]
 
-    li = [(files_train, 'train'), (files_val, 'val')]
+    li = [(files_train, "train"), (files_val, "val")]
 
     # optional test folder
     if use_test:
         files_test = files[split_val:]
-        li.append((files_test, 'test'))
+        li.append((files_test, "test"))
     return li
 
 
@@ -172,8 +178,7 @@ def copy_files(files_type, class_dir, output, prog_bar):
     for (files, folder_type) in files_type:
         full_path = path.join(output, folder_type, class_name)
 
-        pathlib.Path(full_path).mkdir(
-            parents=True, exist_ok=True)
+        pathlib.Path(full_path).mkdir(parents=True, exist_ok=True)
         for f in files:
             if not prog_bar is None:
                 prog_bar.update()
